78cc628810bec04e24cbfabac408c06f21d9d8ba
ydh13886609432 <842502695@qq.com>
2024-05-18 16:15:39 +0800

    init Project

---
+++ Source/GAS_RPGProject/GAS_RPGProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GAS_RPGProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GAS_RPGProject, "GAS_RPGProject" );
